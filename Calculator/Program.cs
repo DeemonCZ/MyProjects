2b50b1cee63adefd2f86fd9f7b978416a10acfc4
Dominik Chuchma <minibiker.d@gmail.com>
2017-07-09 13:19:07 +0200

    Calculator added (hobby project)

---
+++ Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string continueProgram = "y";
+            while (continueProgram == "y")
+            {
+                Console.Clear();
+                Console.WriteLine("Calculator");
+                Console.WriteLine("Insert 1.number, confirm, than insert operand(+ - * /) and 2.number and confirm.");
+
+                int number1;
+                while (!int.TryParse(Console.ReadLine(), out number1))         //test for first number
+                    Console.WriteLine("Wrong number, try again: ");
+
+                char oper = Console.ReadKey().KeyChar;                         //insert operand
+                                                                               //Console.WriteLine();
+
+                int number2;
+                while (!int.TryParse(Console.ReadLine(), out number2))         //test for second number
+                    Console.WriteLine("Wrong number, try again: ");
+
+
+                Computing computing = new Computing();                              //objekt pro počítání
+                Console.WriteLine( computing.Calculation(number1, oper, number2));
+
+
+                Console.WriteLine("Do you want to continue? [y/n]");
+                continueProgram = Console.ReadLine();
+            }
+            Console.WriteLine("End program with any key.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Calculator/computing.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Computing
+    {
+        private bool correctOper = true;            //test for correct "switch" key
+        private int result = 0;
+
+        public string Calculation(int number1, char oper, int number2)
+        {                   
+            switch (oper)
+            {
+                case '+':
+                    {                         //sčítání
+                        result = number1 + number2;
+                        break;
+                    }
+                case '-':
+                    {                         //odčítání
+                        result = number1 - number2;
+                        break;
+                    }
+
+                case '*':
+                    {                         //násobení
+                        result = number1 * number2;
+                        break;
+                    }
+
+                case '/':
+                    {                         //dělení
+                        try { result = number1 / number2; } catch (Exception e) { Console.WriteLine("Dividing with 0 - error"); }   //oeštření pro dělení nulou
+                        break;
+                    }
+
+                default:
+                    {
+                        correctOper = false;
+                        break;
+                    }
+            }
+            //kontrola spravnosti operandu
+            if (correctOper)
+                return string.Format("Result: {0} {1} {2} = {3}", number1, oper, number2, result);
+            else
+                return string.Format("Wrong 'SWITCH' key!");
+
+        }
+    }
+}
